20aff061dd1dc129d2dc7c00a0fbeea6da300057
palmoni5 <d8401948@gmail.com>
2026-05-25 21:50:46 +0300

    מניעת רינדור בחזרה לטאב הפתוח בניהול

--- src/components/providers/SessionProvider.jsx
+++ src/components/providers/SessionProvider.jsx
@@ -3,5 +3,5 @@
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react'
 
 export default function SessionProvider({ children }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+  return <NextAuthSessionProvider refetchOnWindowFocus={false}>{children}</NextAuthSessionProvider>
 }
