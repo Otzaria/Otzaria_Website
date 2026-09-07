f18adeb322d3c11f0bc4418c767e2426e0fe3f62
ClickAndGoScript <click.go.script@gmail.com>
2026-09-08 00:25:46 +0300

    הוספת תשתית Vitest + Testing Library לבדיקות רכיבי React

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  esbuild: {
+    jsx: 'automatic'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+    testTimeout: 15000
+  }
+})
